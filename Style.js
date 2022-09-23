aba8ce2c6eb2d60dd6a35737e25cdbe790136d4d
Cody Mitchell <comitche@redhat.com>
2022-09-23 09:58:14 -0400

    Add expanding section and datepicker to alarms page

--- Style.js
+++ Style.js
@@ -2,14 +2,21 @@ import tw from "twrnc";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    page: [tw`bg-indigo-900 h-full w-full`],
+    page: [tw`h-full w-full` , {
+        backgroundColor: "#1a202c",
+    }],
     loginPage: [tw`bg-indigo-900 h-full items-center justify-center`],
     innerView: [tw`w-full content-center justify-center items-center my-2`],
     text: [tw`text-white text-2xl`],
     subheading: [tw`text-white text-lg`],
     errorText: [tw`text-red-500 text-xl mb-2`],
-    input: [tw`bg-white w-4/5 rounded mb-3 p-2 text-lg`],
-    alarmContainer: [tw`bg-slate-900 rounded-lg p-3 text-lg mb-1 opacity-75 w-100`],
+    input: [tw`bg-white w-full rounded mb-3 p-2 text-lg`],
+    alarmContainer: [tw`bg-black rounded-lg p-3 text-lg mb-1 opacity-60 w-100 border-solid border-2 border-gray-700`],
+    alarmInnerContainer: [{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+    }],
     alarmTitle: [tw`text-white text-xl`],
     alarmSubtitle: [tw`text-white text-sm`],
 });
